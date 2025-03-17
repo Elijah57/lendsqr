66c146e9404e8fec4faca131e37bf46b798b896b
Elijah Echekwu <elijahechekwu30@gmail.com>
2025-03-17 10:12:23 +0100

    dev: update name to asynReqWrapper

--- src/utils/asyncWrapper.ts
+++ src/utils/asyncWrapper.ts
@@ -3,10 +3,8 @@ import { Request, Response, NextFunction } from "express";
 
 type Middlewarefn = (req: Request, res: Response, next: NextFunction)=> Promise<any> | void;
 
-const asyncReqWrapper = (fn:Middlewarefn)=>{
+export const asyncReqWrapper = (fn:Middlewarefn)=>{
     return (req: Request, res: Response, next: NextFunction)=>{
         Promise.resolve(fn(req, res, next)).catch((err)=>next(err))
     }
 }
-
-export default asyncReqWrapper;
