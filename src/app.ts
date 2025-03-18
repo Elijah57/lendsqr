3e6ce5188d52db39c043c1cdd1903459b658a87e
Elijah Echekwu <elijahechekwu30@gmail.com>
2025-03-18 07:15:40 +0100

    refactor: updating file imports

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express"
 import { authRouter, walletRouter } from "./routes"
 import { errorHandler, RouteNotFoundHandler } from "./middlewares/error"
-
+import cors from "cors"
+import configs from "./configs"
 export const app = express()
 app.use(express.json())
+
+app.use(cors(configs.corsOptions))
 
 app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/wallet", walletRouter)
--- src/configs/index.ts
+++ src/configs/index.ts
@@ -2,7 +2,13 @@ const configs = {
     appPort: 6000,
     paystackUrl: "https://api.paystack.co",
     adjutorUrl: "https://adjutor.lendsqr.com/v2/",
-    paystackCallbackUrl: ""
+    paystackCallbackUrl: "",
+    corsOptions:  {
+        origin: "*", // subject to change
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
+      }
 }
 
 export default configs
