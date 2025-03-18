166e67c68de20b1fe5d5d77f781ce91f314e21f6
Elijah Echekwu <elijahechekwu30@gmail.com>
2025-03-18 06:58:59 +0100

    refactor: paystack controller

--- src/controllers/paystack.ts
+++ src/controllers/paystack.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express";
 import paystackApi from "../api/paystackApi";
 import {asyncReqWrapper} from "../utils";
-
-import { verifyPaymentReferenceResponse } from "../types";
+import db from "../db";
 import { BadRequest } from "../middlewares/error";
-import walletService, { WalletService } from "../services/walletService";
+import { IAccountDetails, IBankresolve, ITransferReceipt, ITransferReceiptResponse } from "../types";
+import { fetchBankCode } from "../db/repositories";
 
 
 
@@ -15,13 +15,12 @@ export const initializePayment = asyncReqWrapper(async (req:Request, res: Respon
     const paymentDetails = {
         email: userDetails?.email,
         amount: userDetails?.amount,
-        // callback_url: userDetails?.callbackUrl, // deprecated paystack no longer uses this
+        callback_url: userDetails?.callbackUrl,
         metadata: {
             email: userDetails?.email,
             amount: userDetails?.amount,
             name: userDetails?.name
         }
-
     }
     
     const data = await paystackApi.initializePayment(paymentDetails);
@@ -29,20 +28,51 @@ export const initializePayment = asyncReqWrapper(async (req:Request, res: Respon
 
 });
 
-export const verifyPayment = async (reference: string)=>{
+export const paystackVerifyPayment = async (reference: string)=>{
+    try{
+
+        const response = await paystackApi.verifyPayment(reference);
     
-    const response = await paystackApi.verifyPayment(reference);
-
-    const data = {
-        metadata: response.data.metadata,
-        status: response.data.status,
-        reference: response.data.reference,
-        amount: response.data.amount
+        const data = {
+            metadata: response.data.metadata,
+            status: response.data.status,
+            reference: response.data.reference,
+            amount: response.data.amount
+        }
+    
+        if(data.status !== "success"){
+            throw new BadRequest(`Transaction: ${data.status}`)
+        }
+        return data
+    }catch(error){
+        throw error
     }
-
-    if(data.status !== "success"){
-        throw new BadRequest(`Transaction: ${data.status}`)
-    }
-
-    return data
 };
+
+
+export const paystackResolveBank = async(accountDetails: IAccountDetails)=>{
+    try{
+        
+        const bank = await fetchBankCode(accountDetails.bank_name)
+        if (!bank) throw new Error("Invalid bank");
+        const code = bank?.code
+        const resolve: IBankresolve = {account_number: accountDetails.account_number, bank_code: code}
+        
+        const response = await paystackApi.bankResolve(resolve)
+        response.bank_code = code
+        return response
+    }catch(err){
+        throw err
+    }
+}
+
+export const paystackTransferRecipient = async(transferReciptDetails: ITransferReceipt)=>{
+    try{
+        
+        const response:ITransferReceiptResponse = await paystackApi.createTransferReceipt(transferReciptDetails)
+       
+        return response
+    }catch(err){
+        throw err
+    }
+}
