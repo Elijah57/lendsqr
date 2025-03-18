18822cb20f99dd554fce92e8135f3845b30c9313
Elijah Echekwu <elijahechekwu30@gmail.com>
2025-03-18 03:15:35 +0100

    dev: updated wallet controller (bankResolve)

--- src/controllers/wallet.ts
+++ src/controllers/wallet.ts
@@ -1,11 +1,30 @@
 import { NextFunction, Response, Request } from "express";
-import { verifyPayment } from "./paystack";
+import { paystackResolveBank, paystackVerifyPayment } from "./paystack";
 import walletService from "../services/walletService";
+import paystackApi from "../api/paystackApi";
 import { asyncReqWrapper } from "../utils";
-import { BadRequest, ServerError } from "../middlewares/error";
-import { ITransferDetails } from "../types";
-import { findUserById, getUserWithWallet } from "../db/repositories";
+import { BadRequest, NotFound, ServerError } from "../middlewares/error";
+import { IAccountDetails, IBankResolveResponse, ITransferDetails } from "../types";
+import { findUserById, getUserWallet, getUserWalletTransactions, getUserWithWallet } from "../db/repositories";
 import db from "../db";
+import { error } from "console";
+
+export const getWallet = asyncReqWrapper(async(req: Request, res: Response, next: NextFunction)=>{
+    const userId = req.user?.userId
+    
+    const wallet = await getUserWallet(userId)
+    if(!wallet) throw new NotFound("wallet not found")
+    res.status(200).json({status: true, wallet})
+})
+
+
+export const getTransactions = asyncReqWrapper(async(req: Request, res: Response, next: NextFunction)=>{
+    const userId = req.user?.userId
+    
+    const transactions = await getUserWalletTransactions(userId)
+
+    res.status(200).json({status: true, transactions})
+})
 
 
 export const transferTo = asyncReqWrapper(async(req: Request, res: Response, next: NextFunction)=>{
@@ -23,7 +42,7 @@ export const transferTo = asyncReqWrapper(async(req: Request, res: Response, nex
 
     const data = await walletService.transferToWallet(transferToPayload)
 
-    res.status(200).json({status: true, data})
+    res.status(200).json({status: true, message: "money sent", data})
 })
 
 export const preFundWallet = asyncReqWrapper(async(req: Request, res: Response, next: NextFunction)=>{
@@ -52,7 +71,7 @@ export const verifyFundPayment = asyncReqWrapper(async (req: Request, res: Respo
     
     if(!req.query.reference) throw new BadRequest("Transaction Reference missing")
 
-    const response = await verifyPayment(req.query.reference as string);
+    const response = await paystackVerifyPayment(req.query.reference as string);
 
     const data = {
         metadata: response.metadata,
@@ -84,3 +103,25 @@ export const verifyFundPayment = asyncReqWrapper(async (req: Request, res: Respo
     });
 
 });
+
+
+export const bankResolve = asyncReqWrapper(async (req: Request, res: Response, next: NextFunction)=>{
+    
+    const {account_number, bank_name} = req.body as IAccountDetails
+    const response:IBankResolveResponse = await paystackResolveBank({account_number, bank_name})
+
+    if(!response){
+        return next(new NotFound("Account Details not found"))
+    }
+
+    if(!response.status){
+        throw new NotFound(`Failed to retrieve account details`)
+    }
+   
+    return res.status(200).json({
+        message: response.message,
+        data: response.data
+    });
+
+});
+
