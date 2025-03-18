06ef99369114f02076c4d5dddb345f437f4f677a
Elijah Echekwu <elijahechekwu30@gmail.com>
2025-03-18 07:03:34 +0100

    dev: updated configs variable

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,7 +1,8 @@
 const configs = {
     appPort: 6000,
-    paystackUrl: "",
-    adjutorUrl: "https://adjutor.lendsqr.com/v2/"
+    paystackUrl: "https://api.paystack.co",
+    adjutorUrl: "https://adjutor.lendsqr.com/v2/",
+    paystackCallbackUrl: ""
 }
 
 export default configs
