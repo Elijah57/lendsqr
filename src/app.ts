9e3c797a3b4fca30bc5c230ef267b7e394fab451
Elijah Echekwu <elijahechekwu30@gmail.com>
2025-03-22 22:01:39 +0100

    dev: protect routes with rate-limit

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,23 @@ import express from "express"
 import { authRouter, walletRouter } from "./routes"
 import { errorHandler, RouteNotFoundHandler } from "./middlewares/error"
 import cors from "cors"
+import rateLimit from "express-rate-limit"
 import configs from "./configs"
+
+
 export const app = express()
+
+const authLimiter = rateLimit({
+    windowMs: 10 * 60 * 1000, 
+    max: 5,
+    message: { error: "Too many login attempts, please try again later." },
+});
+
 app.use(express.json())
 
 app.use(cors(configs.corsOptions))
 
-app.use("/api/v1/auth", authRouter)
+app.use("/api/v1/auth", authLimiter, authRouter)
 app.use("/api/v1/wallet", walletRouter)
 
 app.use(RouteNotFoundHandler)
